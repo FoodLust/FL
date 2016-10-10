f616da5fc3bf7b334f114da31fab7be07caaa404
ilikesounds <email_id>
2016-10-10 11:35:18 -0700

    member model is now functioning

--- members/models.py
+++ members/models.py
@@ -6,14 +6,25 @@ from django.utils.encoding import python_2_unicode_compatible
 from django.dispatch import receiver
 
 
+class MemberManager(models.Manager):
+
+    class Meta:
+        model = 'Member'
+
+    def get_queryset(self):
+        qs = super(MemberManager, self).get_query_set()
+        return qs.filter(user__is_active=True)
+
+
 @python_2_unicode_compatible
 class Member(models.Model):
     """ Model for members of the site """
 
     objects = models.Manager()
+    active = MemberManager()
 
     user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
+        User,
         primary_key=True,
         on_delete=models.deletion.CASCADE,
         related_name='member',
@@ -27,3 +38,12 @@ class Member(models.Model):
     @property
     def active(self):
         return self.user.is_active
+
+
+@receiver(models.signals.post_save, sender=User)
+def create_member(sender, **kwargs):
+    try:
+        if kwargs.get('created', True):
+            Member(user=kwargs['instance']).save()
+    except ReferenceError:
+        raise ReferenceError('User not in database.')
