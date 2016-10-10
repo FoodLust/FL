576eb2e7d8a78fce0ef2faff4ef10629ae1c4da7
ilikesounds <email_id>
2016-10-10 11:16:30 -0700

    complete member model and first tests

--- members/models.py
+++ members/models.py
@@ -1,3 +1,29 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
+from django.dispatch import receiver
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Member(models.Model):
+    """ Model for members of the site """
+
+    objects = models.Manager()
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        primary_key=True,
+        on_delete=models.deletion.CASCADE,
+        related_name='member',
+        unique=True
+    )
+
+    def __str__(self):
+        fn = self.user.get_full_name().strip() or self.user.get_username()
+        return '{}'.format(fn)
+
+    @property
+    def active(self):
+        return self.user.is_active
--- members/tests.py
+++ members/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class MemberTestCase(TestCase):
+    """ This class will establish the test cases for the member model """
+
+    def setUp(self):
+        """ Setup for Member model test """
+        self.user = User(username='Test User', first_name='Test')
+        self.user.save()
+
+    def test_user_is_member(self):
+        """ Test to confirm that user is a member """
+        self.assertTrue(hasattr(self.user, 'member'))
