1c5078d91d6c355972c366752ecb23f1bbc9716d
mikekh84 <mikekh@uw.edu>
2016-10-10 18:04:06 -0700

    Changed import urls to import reverse.

--- foodlust/tests.py
+++ foodlust/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.urls import urls
+from django.urls import reverse
 
 class HomeViewTestCase(TestCase):
     """Testcase for home view."""
@@ -7,3 +7,6 @@ class HomeViewTestCase(TestCase):
     def setUp(self):
         """Setup for Home View."""
         self.response = self.client.get(reverse('home'))
+    
+    def test_homeview_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
