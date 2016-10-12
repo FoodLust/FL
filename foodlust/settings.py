66e7404e766a1b9bcf64bb9479a6e797f12fe23e
ilikesounds <email_id>
2016-10-11 18:20:31 -0700

    debug finally fixed

--- foodlust/settings.py
+++ foodlust/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['FL_SECURITY_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = '54.190.47.99'
 
