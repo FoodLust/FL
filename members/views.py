8cf6c191403d939d99bdf699a0623efa741443ec
mikekh84 <mikekh@uw.edu>
2016-10-12 16:49:37 -0700

    Added all meals by user on view.

--- members/views.py
+++ members/views.py
@@ -4,9 +4,11 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def member_view(request):
     """Return a rendered profile view of member."""
+    all_meals = request.user.meal.all()
     return render(request, 'profile.html',context={
         'username': request.user.username,
         'first_name': request.user.first_name,
         'last_name': request.user.last_name,
         'email': request.user.email,
+        'meals': all_meals
     })
