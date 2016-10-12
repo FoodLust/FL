e433dcad77227b9f436626e4e1ba395e2004bc87
mikekh84 <mikekh@uw.edu>
2016-10-12 12:27:12 -0700

    added login required decorator.

--- members/views.py
+++ members/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def member_view(request):
     """Return a rendered profile view of member."""
     return render(request, 'profile.html',context={
