f10ac515169e5f9f7fa52dfa36688210fa2794b9
mikekh84 <mikekh@uw.edu>
2016-10-12 17:20:15 -0700

    Changed profile.html to member.html. And updated views to accrodingly.

--- members/views.py
+++ members/views.py
@@ -3,9 +3,9 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def member_view(request):
-    """Return a rendered profile view of member."""
+    """Return a rendered member view of member."""
     all_meals = request.user.meal.all()
-    return render(request, 'profile.html',context={
+    return render(request, 'member.html',context={
         'username': request.user.username,
         'first_name': request.user.first_name,
         'last_name': request.user.last_name,
