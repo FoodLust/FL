9307a1b1a5ee5811ad0fcde43fd7d32e27ebbb27
ilikesounds <jeffrey.n.torres@gmail.com>
2016-10-14 00:34:18 -0700

    ajax SOOOO close

--- foodlust/static/js/ajax_listener.js
+++ foodlust/static/js/ajax_listener.js
@@ -1,20 +1,32 @@
-// json_data = json.dumps({'new_rating': '78'})
-// HTTPRsponse(json_data, content_type='application/json')
-
-$('.liked').on('click', function(e) {
+$('.selector').on('click', '.liked-blue', function(e) {
   e.preventDefault();
   var id = e.target.href.split('meals')[1].split('/')[1];
   $.get('http://localhost:8000/meals/' + id + '/liked').done(function(data){
-    $('.rating span.'+id).html(data)
-
+    $('.liked-blue').removeAttr('href');
+    $('.liked-blue').addClass('liked-gray');
+    $('.liked-blue').removeClass('liked-blue');
+    $('.liked-gray').css('color', 'gray');
+    $('.disliked-gray').attr('href', ("/meals/" + id + "/disliked"));
+    $('.disliked-gray').removeAttr('style');
+    $('.disliked-gray').addClass('unliked-blue');
+    $('.disliked-gray').removeClass('unliked-gray');
+    $('.rating span.'+id).html(data);
   })
 })
 
-$('.disliked').on('click', function(e) {
+$('.selector').on('click', '.disliked-gray', function(e) {
   e.preventDefault();
   var id = e.target.href.split('meals')[1].split('/')[1];
   $.get('http://localhost:8000/meals/' + id + '/disliked').done(function(data) {
-      $('.rating span.'+id).html(data)
+        $('.disliked-blue').removeAttr('href');
+        $('.disliked-blue').addClass('liked-gray');
+        $('.disliked-blue').removeClass('liked-blue');
+        $('.disliked-gray').css('color', 'gray');
+        $('.liked-gray').attr('href', ("/meals/" + id + "/disliked"));
+        $('.liked-gray').removeAttr('style');
+        $('.liked-gray').addClass('unliked-blue');
+        $('.liked-gray').removeClass('unliked-gray');
+        $('.rating span.'+id).html(data);
 
   })
 })
